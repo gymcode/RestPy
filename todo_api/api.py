0099940d93df66a3e0254cfd0e217b901a41d18f
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-09-20 23:54:27 +0000

    testing en evaluating

--- todo_api/api.py
+++ todo_api/api.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 #  initialize the apis
 api = Api(app)
 
+class HelloWorld (Resource):
+    def get(self): 
+        return {"data": "hello, Kenneth"}
 
 # creating end points 
-api.add_resource(Helloworld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld")
 api.add_resource(HelloName, "/helloname/<string:name>")
 
 if __name__ == "__main__": 
